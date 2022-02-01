89b2ae051ac44c6a2a0bb662f06b4bbca8fefc69
G-M0NEY <grantjefferson1@gmail.com>
2022-01-31 19:01:14 -0700

    rows

--- steam-react/src/components/CrushBoard.js
+++ steam-react/src/components/CrushBoard.js
@@ -28,6 +28,20 @@ const CrushBoard = () => {
             }
         }
     }
+
+    const checkForRowOfFour = () => {
+        for (let i = 0; i <= 47; i++) {
+            const rowOfFour = [i, i + 1, i + 2, i +3]
+            const decidedColor = currentColorArrangment[i]
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+
+            if (notValid.includes(i)) continue
+
+            if( rowOfFour.every(square => currentColorArrangment[square] === decidedColor)) {
+                rowOfFour.forEach(square => currentColorArrangment[square] = '')
+            }
+        }
+    }
     
     const checkForColumnOfThree = () => {
         for (let i = 0; i <= 47; i++) {
@@ -39,7 +53,40 @@ const CrushBoard = () => {
             }
         }
     }
+
+    const checkForRowOfThree = () => {
+        for (let i = 0; i <= 47; i++) {
+            const rowOfThree = [i, i + 1, i + 2]
+            const decidedColor = currentColorArrangment[i]
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+            if (notValid.includes(i)) continue
+
+            if( rowOfThree.every(square => currentColorArrangment[square] === decidedColor)) {
+                rowOfThree.forEach(square => currentColorArrangment[square] = '')
+            }
+        }
+    }
     
+    const moveIntoSquareBelow = () => {
+        for (let i = 0; i < 64 - width; i++) {
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+            const isFirstRow = firstRow.includes(i)
+            
+            if (isFirstRow && currentColorArrangment[i] === '') {
+                let randomNumber = Math.floor(Math.random() * candyColors.length)
+                currentColorArrangment[i] = candyColors[randomNumber]
+            }
+
+            if (currentColorArrangment[i + width] === '') {
+                currentColorArrangment[i + width] = currentColorArrangment[i]
+                currentColorArrangment[i] = ''
+            }
+        }
+    }
+
+    
+     
 
     const createBoard = () => {
         const randomColorArrangment = []
@@ -57,11 +104,14 @@ const CrushBoard = () => {
     useEffect (() => {
         const timer = setInterval(() => {
             checkForColumnOfFour()
+            checkForRowOfFour()
             checkForColumnOfThree()
+            checkForRowOfThree()
+            moveIntoSquareBelow()
             setCurrentColorArrangement([...currentColorArrangment])
         }, 100)
         return () => clearInterval(timer)
-    }, [checkForColumnOfFour, checkForColumnOfThree,  currentColorArrangment])
+    }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, moveIntoSquareBelow,  currentColorArrangment])
 
 
 
@@ -74,6 +124,14 @@ const CrushBoard = () => {
                  key={index}
                  style={{backgroundColor: candyColor}}
                  alt={candyColor}
+                 data-id={index}
+                 draggable={true}
+                 onDragStart={dragStart}
+                 onDragOver={(e) => e.preventDefault()}
+                 onDragEnter={(e) => e.preventDefault()}
+                 onDragLeave={(e) => e.preventDefault()}
+                 onDrop={dragDrop}
+                 onDragEnd={dragEnd}
                 />
             ))}
         </div>
