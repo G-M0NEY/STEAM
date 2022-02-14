4faa5eaed1cb9884756c961a8e469534f2231146
G-M0NEY <grantjefferson1@gmail.com>
2022-02-13 18:10:39 -0700

    game

--- steam-react/src/components/CrushBoard.js
+++ steam-react/src/components/CrushBoard.js
@@ -40,24 +40,6 @@ const CrushBoard = () => {
                 }
             }
         }
-<<<<<<< HEAD
-    }
-
-    const checkForRowOfFour = () => {
-        for (let i = 0; i <= 47; i++) {
-            const rowOfFour = [i, i + 1, i + 2, i +3]
-            const decidedColor = currentColorArrangment[i]
-            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
-
-            if (notValid.includes(i)) continue
-
-            if( rowOfFour.every(square => currentColorArrangment[square] === decidedColor)) {
-                rowOfFour.forEach(square => currentColorArrangment[square] = '')
-            }
-        }
-    }
-=======
->>>>>>> 8ce694780414f689c84c794e6637bed0cee0b8c2
     
         const checkForRowOfFour = () => {
             for (let i = 0; i < 64; i++) {
@@ -75,69 +57,6 @@ const CrushBoard = () => {
                 }
             }
         }
-<<<<<<< HEAD
-    }
-
-    const checkForRowOfThree = () => {
-        for (let i = 0; i <= 47; i++) {
-            const rowOfThree = [i, i + 1, i + 2]
-            const decidedColor = currentColorArrangment[i]
-            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
-
-            if (notValid.includes(i)) continue
-
-            if( rowOfThree.every(square => currentColorArrangment[square] === decidedColor)) {
-                rowOfThree.forEach(square => currentColorArrangment[square] = '')
-            }
-        }
-    }
-    
-    const moveIntoSquareBelow = () => {
-        for (let i = 0; i < 64 - width; i++) {
-            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
-            const isFirstRow = firstRow.includes(i)
-            
-            if (isFirstRow && currentColorArrangment[i] === '') {
-                let randomNumber = Math.floor(Math.random() * candyColors.length)
-                currentColorArrangment[i] = candyColors[randomNumber]
-            }
-
-            if (currentColorArrangment[i + width] === '') {
-                currentColorArrangment[i + width] = currentColorArrangment[i]
-                currentColorArrangment[i] = ''
-            }
-        }
-    }
-
-    
-     
-
-    const createBoard = () => {
-        const randomColorArrangment = []
-        for (let i=0; i < width * width; i++) {
-            const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
-            randomColorArrangment.push(randomColor)
-        }
-        setCurrentColorArrangement(randomColorArrangment)
-    }
-
-    useEffect(() => {
-        createBoard()
-    }, [])
-
-    useEffect (() => {
-        const timer = setInterval(() => {
-            checkForColumnOfFour()
-            checkForRowOfFour()
-            checkForColumnOfThree()
-            checkForRowOfThree()
-            moveIntoSquareBelow()
-            setCurrentColorArrangement([...currentColorArrangment])
-        }, 100)
-        return () => clearInterval(timer)
-    }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, moveIntoSquareBelow,  currentColorArrangment])
-
-=======
     
         const checkForColumnOfThree = () => {
             for (let i = 0; i <= 47; i++) {
@@ -229,7 +148,6 @@ const CrushBoard = () => {
               }
             }
           }
->>>>>>> 8ce694780414f689c84c794e6637bed0cee0b8c2
 
           const createBoard = () => {
             setCurrentColorArrangement(
@@ -257,9 +175,9 @@ const CrushBoard = () => {
     
 
   return (
-    <div className='container'>
+    
     <div className='loveCrush'>
-        <div className='game'>
+        <div className='game' >
             {currentColorArrangement.map((candyColor, index) => (
                  <img
                  key={index}
@@ -273,16 +191,10 @@ const CrushBoard = () => {
                  onDragLeave={(e) => e.preventDefault()}
                  onDrop={dragDrop}
                  onDragEnd={dragEnd}
-<<<<<<< HEAD
-                />
-=======
              />
->>>>>>> 8ce694780414f689c84c794e6637bed0cee0b8c2
             ))}
         </div>
         <ScoreBoard score={scoreDisplay}/>
-
-    </div>
     </div>
   )
 };
--- steam-react/src/components/ScoreBoard.js
+++ steam-react/src/components/ScoreBoard.js
@@ -1,7 +1,9 @@
+import "./components.css";
+
 const ScoreBoard = ({ score }) => {
     return (
       <div className="score-board">
-        <h2>{score}</h2>
+        <h2>Score={score}</h2>
       </div>
     )
   }
